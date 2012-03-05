407994383e464b2c9b5bddd44ffae9a7c5a096c9
ritchie <blackdog@cloudshift.cl>
2012-03-05 07:59:48 -0300

    create this proxy that SessionMgr inherits from, so that the client remoting proxy doesn't include any server side refs

---
+++ cloudshift/session/SessionMgrProxy.hx
@@ -0,0 +1,18 @@
+
+package cloudshift.session;
+
+import cloudshift.Session;
+
+class SessionMgrProxy {
+  public function
+  login(pkt:Dynamic,cb:ESession->Void):Void {}
+
+  public function
+  signup(pkt:Dynamic,cb:ESession->Void):Void {}
+
+  public function
+  logout(sessID:String,cb:ESession->Void):Void {}
+}
+
+
+
